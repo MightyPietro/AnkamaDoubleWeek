d938a48a4baef1f10cd040c59b060f65c84813f4
Quentin Milana <quentin.milana@hotmail.fr>
2021-03-03 12:21:31 +0100

    Player and Player Manager OK

---
+++ Assets/Features/Action/Scripts/Effects/Damage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WeekAnkama
+{
+    public class Damage : ActionEffect
+    {
+        public override void Process()
+        {
+            Debug.Log("Damage");
+        }
+
+    }
+}
+
--- Assets/Features/Player/Scripts/PlayerManager.cs
+++ Assets/Features/Player/Scripts/PlayerManager.cs
@@ -1,20 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Sirenix.OdinInspector;
+using UnityEngine.UI;
 namespace WeekAnkama
 {
     public class PlayerManager : MonoBehaviour
     {
-        private Player _actualPlayer;
+        #region Private Variables
+        [SerializeField] private Player _actualPlayer;
+        [SerializeField] private Transform _cardsLayout;
+        [SerializeField] private Button _actionButtonPrefab;
+        #endregion
 
         #region Getter/Setter
         public Player actualPlayer { get { return _actualPlayer; } set { _actualPlayer = value; } }
+        public List<Action> actualPlayerHand { get { return actualPlayer.hand; } set { actualPlayer.hand = value; } }
         #endregion
 
-        public void DoAction(Action action)
+        public void StartPlayerTurn(Player _setActualPlayer)
         {
-            action.Process();
+            actualPlayer = _setActualPlayer;
+            DoDraw();
+            DisplayCards();
+        }
+
+        [Button]
+        private void DoAction()
+        {
+            actualPlayer.currentAction.Process();
+        }
+
+        [Button]
+        private void DoDraw()
+        {
+            actualPlayer.hand.Clear();
+            actualPlayer._deckReminder.Clear();
+            for (int i = 0; i < actualPlayer.deck.Count; i++)
+            {
+                actualPlayer._deckReminder.Add(actualPlayer.deck[i]);
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                int rand = Random.Range(0, actualPlayer._deckReminder.Count);
+                actualPlayer.hand.Add(actualPlayer._deckReminder[rand]);
+                actualPlayer._deckReminder.Remove(actualPlayer._deckReminder[rand]);
+
+            }
+        }
+
+        private void AddCurrentAction(Action action)
+        {
+            actualPlayer.currentAction = action;
+        }
+
+        [Button]
+        private void DisplayCards()
+        {
+            for (int i = 0; i < actualPlayer.hand.Count; i++)
+            {
+                Button _instantiatedActionButton = Instantiate(_actionButtonPrefab, _cardsLayout);
+                Action action = actualPlayer.hand[i];
+                _instantiatedActionButton.onClick.AddListener(() => AddCurrentAction(action));
+                _instantiatedActionButton.name = action.name;
+                _instantiatedActionButton.GetComponentInChildren<Text>().text = action.name;
+            }
+
         }
 
     }
