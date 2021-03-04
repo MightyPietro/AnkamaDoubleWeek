ca2099f92286e041bce41d80f656a6e899343d3f
Quentin Milana <quentin.milana@hotmail.fr>
2021-03-04 10:56:06 +0100

    Add some actions

---
+++ Assets/Features/Action/Scripts/Effects/Water.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WeekAnkama
+{
+    public class Water : ActionEffect
+    {
+        public override void Process(Tile targetTile, Action action)
+        {
+            Instantiate(action.prefab, targetTile.WorldPosition,Quaternion.identity);
+        }
+    }
+}
+
