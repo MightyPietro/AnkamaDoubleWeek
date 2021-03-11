4b5fc3c1b11ae5649f9ac7627bcd63907ddc1b17
Antoine MACHINET <a.machinet@rubika-edu.com>
2021-03-11 15:13:46 +0100

    Flame VFX

---
+++ Assets/Shader/CameraLookAt.cs
@@ -0,0 +1,17 @@
+﻿//	CameraFacing.cs 
+//	original by Neil Carter (NCarter)
+//	modified by Hayden Scott-Baron (Dock) - http://starfruitgames.com
+//  allows specified orientation axis
+
+
+using UnityEngine;
+using System.Collections;
+
+var cameraToLookAt: Camera;
+     
+    function Update()
+{
+    var v: Vector3 = cameraToLookAt.transform.position - transform.position;
+    v.x = v.z = 0.0;
+    transform.LookAt(cameraToLookAt.transform.position - v);
+}
